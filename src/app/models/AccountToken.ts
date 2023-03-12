645f2f958e883baa1c482d75156b2eddf582f311
pocytamy <phamvantuan.133.1996@gmail.com>
2023-03-13 04:15:46 +0700

    check block login

--- src/app/models/AccountToken.ts
+++ src/app/models/AccountToken.ts
@@ -1,12 +1,15 @@
 import {Role} from "./Role";
 
 export class AccountToken {
+    id!:number;
     username: string;
     token: string;
 
     roles: Role[];
 
-    constructor(username: string, token: string, roles: Role[]) {
+
+    constructor(id: number, username: string, token: string, roles: Role[]) {
+        this.id = id;
         this.username = username;
         this.token = token;
         this.roles = roles;
